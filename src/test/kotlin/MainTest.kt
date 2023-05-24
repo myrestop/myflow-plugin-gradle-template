a88ed5c579e39a368c961b0d75235545a591031c
easepan <easepan@qq.com>
2023-05-24 16:09:19 +0800

    feat: add run code

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,12 @@
+import top.myrest.myflow.AppInfo
+import top.myrest.myflow.baseimpl.App
+import top.myrest.myflow.baseimpl.FlowApp
+import top.myrest.myflow.baseimpl.enableDevEnv
+import top.myrest.myflow.dev.DevProps
+
+fun main() {
+    enableDevEnv()
+    DevProps.disableNativeListener = false
+    FlowApp().configApp()
+    App(AppInfo.appName + "Xxx")
+}
