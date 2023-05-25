3fc643832fd5405a5a7ed547a2c869590261a3a7
easepan <easepan@qq.com>
2023-05-25 12:03:35 +0800

    feat: optimize run code

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -7,6 +7,7 @@ import top.myrest.myflow.dev.DevProps
 fun main() {
     enableDevEnv()
     DevProps.disableNativeListener = false
+    AppInfo.actionWindow.isUndecorated = false
     FlowApp().configApp()
     App(AppInfo.appName + "Xxx")
 }
