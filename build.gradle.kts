44089fcf5945cf7aa3c14cc00d03895fd8ac3df9
easepan <easepan@qq.com>
2024-04-03 11:08:45 +0800

    feat: update myflow version to v1.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
     google()
 }
 
-val myflowVersion = "1.0.11"
+val myflowVersion = "1.0.12"
 
 var myflowDependency: Dependency? = null
 var jetbrainsComposeDependency: Dependency? = null
